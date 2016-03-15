a368b42f9d2b38d4a81c299cf9af6f09d1ad2ce5
Marvin <marvin@codeforprogress.org>
2016-03-15 11:05:52 -0400

    added p

---
+++ pw.py
@@ -0,0 +1,7 @@
+# dbpass  = 'dbpass'
+googMapKey = 'AIzaSyATHC7NschEdRyTTlu5RxVWXT4YTYRPrFs'
+
+AED = 'd73vgqjq6cl4lr'
+postgres = 'gdzwpdhassdkgp'
+dbpass = 'nSbqtdY9eHHOVOGAmiHVOE_3s7'
+host = 'ec2-54-227-250-148.compute-1.amazonaws.com'
