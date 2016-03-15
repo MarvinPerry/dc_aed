b50cbe7202e4fd12168c85314d25fbad6c7134a0
Marvin <marvin@codeforprogress.org>
2016-03-15 11:35:34 -0400

    fix

--- pw.py
+++
@@ -1,7 +0,0 @@
-# dbpass  = 'dbpass'
-googMapKey = 'AIzaSyATHC7NschEdRyTTlu5RxVWXT4YTYRPrFs'
-
-AED = 'd73vgqjq6cl4lr'
-postgres = 'gdzwpdhassdkgp'
-dbpass = 'nSbqtdY9eHHOVOGAmiHVOE_3s7'
-host = 'ec2-54-227-250-148.compute-1.amazonaws.com'
